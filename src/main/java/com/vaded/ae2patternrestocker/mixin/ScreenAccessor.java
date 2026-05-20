104b87dfee34cca352ac6009d407dd60f98e10e0
Vade <kjstacey@gmail.com>
2026-05-20 16:56:05 -0500

    Fix ScreenAccessor @Invoker erased type mismatch
    addRenderableWidget has generic bound <T extends GuiEventListener & ...>,
    so T erases to GuiEventListener, not AbstractWidget. The @Invoker was
    matching the wrong bytecode descriptor and crashing with
    InvalidAccessorException.

--- src/main/java/com/vaded/ae2patternrestocker/mixin/ScreenAccessor.java
+++ src/main/java/com/vaded/ae2patternrestocker/mixin/ScreenAccessor.java
@@ -1,6 +1,8 @@
 package com.vaded.ae2patternrestocker.mixin;
 
-import net.minecraft.client.gui.components.AbstractWidget;
+import net.minecraft.client.gui.components.Renderable;
+import net.minecraft.client.gui.components.events.GuiEventListener;
+import net.minecraft.client.gui.narration.NarratableEntry;
 import net.minecraft.client.gui.screens.Screen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Invoker;
@@ -8,5 +10,5 @@ import org.spongepowered.asm.mixin.gen.Invoker;
 @Mixin(Screen.class)
 public interface ScreenAccessor {
     @Invoker("addRenderableWidget")
-    <T extends AbstractWidget> T ae2PatternRestocker$addRenderableWidget(T widget);
+    <T extends GuiEventListener & Renderable & NarratableEntry> T ae2PatternRestocker$addRenderableWidget(T widget);
 }
